63e1c4bb91cc7dfb569141b8765c6fd819c6bf0a
Furkan <furkan@globalbridge.com>
2026-03-27 05:46:26 +0300

    fix: revert to middleware.ts for correct next-intl locale routing

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,7 @@
 import createMiddleware from 'next-intl/middleware'
 import { routing } from './i18n/routing'
 
-export function proxy(request: Request) {
-  return createMiddleware(routing)(request as any)
-}
+export default createMiddleware(routing)
 
 export const config = {
   matcher: ['/((?!api|_next|_vercel|.*\\..*).*)'],
